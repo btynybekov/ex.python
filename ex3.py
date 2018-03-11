5b655dfc178c3a35c06fa0c6e74aa2f46ffaecc1
btynybekov <btynybekovt@gmail.com>
2018-03-12 00:13:05 +0600

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -8,14 +8,12 @@ def compute():
 			return str(n)
 
 
-# Returns the smallest factor of n, which is in the range [2, n]. The result is always prime.
 def smallest_prime_factor(n):
 	assert n >= 2
 	for i in range(2, eulerlib.sqrt(n) + 1):
 		if n % i == 0:
 			return i
-	return n  # n itself is prime
-
+	return n  
 
 if __name__ == "__main__":
 	print(compute())
